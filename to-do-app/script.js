1f6b96dfab93a04d73ddcc7567ee72e26845782d
unknown <adithkrishnaor@gmail.com>
2025-07-21 17:54:50 +0530

    to-do-app add,delete done

--- to-do-app/script.js
+++ to-do-app/script.js
@@ -1,4 +1,4 @@
-const tasks = [];
+let tasks = [];
 const taskInput = document.getElementById("taskInput");
 const addTask = document.getElementById("addTask");
 const taskList = document.getElementById("taskList");
@@ -20,7 +20,18 @@ function renderTask() {
     const li = document.createElement("li");
     li.className = "bg-gray-200 mt-1";
     li.innerHTML = `
-    <span>${task.text}</span>`;
+    <span>${task.text}</span>
+    <div>
+        <button class="delete bg-red-500 text-white px-2 py-1 rounded hover:bg-red-700" data-id="${task.id}">Delete</button>
+    </div>`;
     taskList.appendChild(li);
   });
 }
+
+taskList.addEventListener("click", (e) => {
+  const id = e.target.dataset.id;
+  if (e.target.classList.contains("delete")) {
+    tasks = tasks.filter((task) => task.id != id);
+  }
+  renderTask();
+});
