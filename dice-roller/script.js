9668f2609e03bd23f2d68614b74ba5ffc46882be
unknown <adithkrishnaor@gmail.com>
2025-07-23 12:23:47 +0530

    added img in roll dice

--- dice-roller/script.js
+++ dice-roller/script.js
@@ -1,10 +1,10 @@
 const roll = document.getElementById("roll");
-const result1 = document.getElementById("result1");
-const result2 = document.getElementById("result2");
+const img1 = document.getElementById("dice-1");
+const img2 = document.getElementById("dice-2");
 roll.addEventListener("click", () => {
   console.log("Roll clicked");
   const randomNum1 = Math.floor(Math.random() * 6) + 1;
-  result1.textContent = randomNum1;
+  img1.src = `../assets/dice-${randomNum1}.png`;
   const randomNum2 = Math.floor(Math.random() * 6) + 1;
-  result2.textContent = randomNum2;
+  img2.src = `../assets/dice-${randomNum2}.png`;
 });
